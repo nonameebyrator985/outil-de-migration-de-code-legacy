0de0b3580b6cfef25f1e671bac71726a734f5be1
nonameebyrator985 <singheingemo@hotmail.com>
2025-11-08 22:11:15 +0900

    feat: initial commit for converter.js

---
+++ converter.js
@@ -0,0 +1,8 @@
+function convertCode(oldCode) {
+    // Logique de conversion du code ancien ici
+    console.log(`Conversion du code : ${oldCode}`);
+    // Simuler une conversion
+    return 'nouveauCode';
+}
+
+module.exports = convertCode;
