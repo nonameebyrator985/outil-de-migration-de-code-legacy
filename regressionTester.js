ce4414cdf51e0140fc21487ec0ffca7a34d88de1
nonameebyrator985 <singheingemo@hotmail.com>
2025-11-08 22:11:17 +0900

    feat: initial commit for regressionTester.js

---
+++ regressionTester.js
@@ -0,0 +1,6 @@
+function testRegression(newVersion) {
+    // Logique de test de régression ici
+    console.log(`Test de régression pour la version : ${newVersion}`);
+}
+
+module.exports = testRegression;
